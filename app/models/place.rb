85ef405509d9f33a5954388a3204e46f0b6682c5
Russell Kranz <russkranz@yahoo.com>
2016-11-10 07:23:06 +0000

    added geocoding

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,10 @@
 class Place < ActiveRecord::Base
     self.per_page = 3
     belongs_to :user
-    validates :name, presence: true
+
+      geocoded_by :address
+      after_validation :geocode
+
+    validates :name, :address, :description, presence: true
+    validates :name, length: { minimum: 3 }
 end
